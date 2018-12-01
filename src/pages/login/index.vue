b492cce65e0d5872418323948bb2a0d9751fa375
Ailences <xs3646503@gmail.com>
2018-12-01 19:27:07 +0800

    初步实现登录

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,14 +1,56 @@
 <template>
-    <div class="login-container">
-        <h1>这是登录页面</h1>
-    </div>
+  <div class="login-container">
+    <h1 class="title">用户登录</h1>
+    <el-form ref="form" label-width="80px">
+      <el-form-item label="用户名:">
+        <el-input v-model="username"></el-input>
+      </el-form-item>
+      <el-form-item label="密　码:">
+        <el-input type="password" v-model="password"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" class="login-btn" @click="handleLogin">立即注册</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
-    
+import axios from "axios";
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    handleLogin() {
+      console.log("执行登录操作!");
+      axios.post('http://127.0.0.1:8888/api/private/v1/login',{
+          username: this.username,
+          password: this.password
+      }).then(res =>{
+          console.log(res);
+      });
+    }
+  }
+};
 </script>
 
 
 <style lang="less">
-
+.login-container {
+  width: 500px;
+  margin: 150px auto;
+  .title {
+    text-align: center;
+    margin: 20px 0;
+    font-size: 24px;
+  }
+  .login-btn {
+    width: 100%;
+  }
+}
 </style>
